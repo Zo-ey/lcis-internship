ee4208c3821e53987eabdb63e9dee265ea980d8e
Zoé Lagache <lagache@ug.kth.se>
2023-04-18 18:49:04 +0200

    Added link source addresses management in tests and messages class

--- mesa-code/wsn_message.py
+++ mesa-code/wsn_message.py
@@ -5,9 +5,11 @@ BROADCAST = -1
 WSNMessageType = Enum("WSNMessage", ("Data", "Advertisement"))
 
 class WSNMessage:
-    def __init__(self, src=None, dest=None, message_type=None, data=None):
+    def __init__(self, src=None, dest=None, message_type=None, data=None, src_link=None, dest_link=None):
         self.src = src
         self.dest = dest
+        self.src_link = src_link
+        self.dest_link = dest_link
         self.type = WSNMessageType[message_type]
         self.data = data
     
@@ -15,7 +17,9 @@ class WSNMessage:
         ret = 'src : '+str(self.src) + ", "
         ret += 'dest : '+str(self.dest) + ", "
         ret += 'message_type : '+str(self.type) + ", "
-        ret += 'data : '+str(self.data)
+        ret += 'data : '+str(self.data) + ", "
+        ret += 'src_link : '+str(self.src_link) + ", "
+        ret += 'dest_link : '+str(self.dest_link)
         return ret
 
     def __eq__(self, other):
